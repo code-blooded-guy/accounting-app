27ad707c0360052e47cad362cfe739ec7e4e0c99
Paras <paraspatel1434@gmail.com>
2022-11-02 12:23:43 +0530

    status api

--- src/business/status.business.js
+++ src/business/status.business.js
@@ -11,7 +11,27 @@ const getAllLogged = async (user_id) => {
   return await StatusModel.find({ user_id });
 };
 
+const add = async (body) => {
+  // Database query
+  return await StatusModel.create(body);
+};
+
+const update = async (body ,params) => {
+  // Database query
+  return await StatusModel.findOneAndUpdate({_id:params.id},body);
+};
+
+const Delete = async (body,params) => {
+  // Database query
+  // var data = await CatagoryModel.find(body);
+  // return data
+  return await StatusModel.findOneAndRemove({_id:params.id},body);
+};
+
 export default {
   getAll,
-  getAllLogged
+  getAllLogged,
+  add,
+  update,
+  Delete
 };
